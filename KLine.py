e99bc40e55fcff46211a6125ac4c7d5865419124
skywalker <512559733@qq.com>
2023-02-22 15:36:06 +0800

    1.KLine例子改为读取本地数据 2.增加了README

--- KLine.py
+++ KLine.py
@@ -1,18 +1,33 @@
 import mplfinance as mpf
-
+from pandas import read_csv, to_datetime
+from pathlib import Path
 from Stock import Stock
+
 
 if __name__ == '__main__':
     # 初始化
     stock_number = 'sz000001'
     stock = Stock(stock_number)
+
     # 指定时间段
     period = Stock.DAY
-    data = stock.get_stock_data(period)
+    # data = stock.get_stock_data(period)
+
     # 转为DataFrame
-    df = Stock.to_dataframe(data)
-    # 写入csv
-    # df.to_csv(f'{stock_number}_{Stock.PERIOD_LIST[period]}.csv')
+    # df = Stock.to_dataframe(data)
+
+    # 设置路径
+    file_path = Path('data')
+    file_path.mkdir(exist_ok=True)
+    file_path = file_path.joinpath(f'{stock_number}_{Stock.PERIOD_LIST[period]}.csv')
+
+    # # 写入csv
+    # df.to_csv(file_path)
+
+    # 读取csv
+    df = read_csv(file_path)
+    df.set_index(['date'], inplace=True)
+    df.index = to_datetime(df.index)
 
     # 设置K线样式
     my_colors = mpf.make_marketcolors(up='r',
